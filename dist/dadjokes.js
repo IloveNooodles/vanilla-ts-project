5e414f51ffa494f7d2288d8b843723c62ca81eee
IloveNooodles <mgarebaldhie80@gmail.com>
2022-06-11 00:01:32 +0700

    feat: dad jokes

--- dist/dadjokes.js
+++ dist/dadjokes.js
@@ -1,6 +1,28 @@
 "use strict";
-const url = "./data.json";
-fetch(url)
-    .then((resp) => resp.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
+const url = "https://icanhazdadjoke.com/";
+const dadJokesBtn = document.querySelector(".btn");
+const pResult = document.querySelector(".result");
+dadJokesBtn?.addEventListener("click", () => {
+    fetchRandomJokes();
+});
+const fetchRandomJokes = async () => {
+    pResult.textContent = "loading";
+    try {
+        const response = await fetch(url, {
+            headers: {
+                Accept: "application/json",
+            },
+        });
+        if (!response.ok) {
+            pResult.textContent = "error";
+            throw new Error("Response error");
+        }
+        const dataRes = await response.json();
+        pResult.textContent = dataRes.joke;
+    }
+    catch (err) {
+        pResult.textContent = "there was an error";
+        console.log(err);
+    }
+};
+fetchRandomJokes();
--- ts/dadjokes.ts
+++ ts/dadjokes.ts
@@ -1,6 +1,29 @@
-const url: string = "./data.json";
+const url: string = "https://icanhazdadjoke.com/";
 
-fetch(url)
-  .then((resp) => resp.json())
-  .then((data) => console.log(data))
-  .catch((err: Error) => console.log(err));
+const dadJokesBtn = document.querySelector(".btn") as HTMLElement;
+const pResult = document.querySelector(".result") as HTMLParagraphElement;
+dadJokesBtn?.addEventListener("click", () => {
+  fetchRandomJokes();
+});
+
+const fetchRandomJokes = async () => {
+  pResult.textContent = "loading";
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    if (!response.ok) {
+      pResult.textContent = "error";
+      throw new Error("Response error");
+    }
+    const dataRes = await response.json();
+    pResult.textContent = dataRes.joke;
+  } catch (err: any) {
+    pResult.textContent = "there was an error";
+    console.log(err);
+  }
+};
+
+fetchRandomJokes();
