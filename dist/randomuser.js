d5d03eb3790c6ef848a3dd221e5d05c7b7d467ea
IloveNooodles <mgarebaldhie80@gmail.com>
2022-06-11 15:42:33 +0700

    feat: randomUser complete

---
+++ dist/randomuser.js
@@ -0,0 +1,56 @@
+"use strict";
+const randomUserURL = "https://randomuser.me/api/";
+const getElementUser = (selectedElement) => {
+    const element = document.querySelector(selectedElement);
+    if (element)
+        return element;
+    throw new Error("No element selected");
+};
+const img = getElementUser(".user-image");
+const title = getElementUser(".user-title");
+const value = getElementUser(".user-value");
+const btn = getElementUser(".btn");
+const btns = [...document.querySelectorAll(".icon")];
+const getUser = async () => {
+    const response = await fetch(randomUserURL);
+    const responseData = await response.json();
+    const person = responseData.results[0];
+    const { phone, email } = person;
+    const { large: image } = person.picture;
+    const { password } = person.login;
+    const { first, last } = person.name;
+    const { dob: { age }, } = person;
+    const { street: { number, name }, } = person.location;
+    return {
+        phone,
+        email,
+        image,
+        password,
+        first,
+        age,
+        street: `${number} ${name}`,
+        name: `${first} ${last}`,
+    };
+};
+const showRandomUser = async () => {
+    const randomUser = await getUser();
+    displayUser(randomUser);
+};
+const displayUser = (person) => {
+    img.src = person.image;
+    value.textContent = person.name;
+    title.textContent = `My name is`;
+    btns.forEach((button) => button.classList.remove("active"));
+    btns[0].classList.add("actice");
+    btns.forEach((btn) => {
+        const label = btn.dataset.label;
+        btn.addEventListener("click", () => {
+            title.textContent = `My ${label} is`;
+            value.textContent = person[label];
+            btns.forEach((button) => button.classList.remove("active"));
+            btn.classList.add("active");
+        });
+    });
+};
+window.addEventListener("DOMContentLoaded", showRandomUser);
+btn.addEventListener("click", showRandomUser);
